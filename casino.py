621c8f57e68bef37679d40a555d7b9eb8c16f379
aldhi12 <70382948+aldhi12@users.noreply.github.com>
2020-10-08 21:54:47 +0700

    Update casino.py

--- casino.py
+++ casino.py
@@ -8,21 +8,15 @@ api_hash = environ['hash']
 string = environ['string1']
 client = TelegramClient(StringSession(string), api_id, api_hash)
 
-bot = 'kampungmaifamx4bot'	
+bot = 'kampungmaifamA2bot'	
 async def main():
 	async with client:
 		while(True):
 			num = random.randint(1, 6)
-			message = '/casino_FortuneDice_{}_5e10'.format(num)
+			message = '/casino_Fiftyfifty_{}_5e10'.format(num)
 			await client.send_message(bot, message)
 			print(message)
-			
-			async for pesan in client.iter_messages(entity=bot, limit=1):
-				check = pesan.text
-				if 'level increased' in check:
-					await client.send_message(bot, message)
-					print('Eat')
-			await asyncio.sleep(301)
+			await asyncio.sleep(241)
 			await client.send_message(bot, '/casino_result')
 			await asyncio.sleep(3)
 client.loop.run_until_complete(main())
